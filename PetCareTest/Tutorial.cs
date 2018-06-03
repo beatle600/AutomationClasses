aba04586f9ef3e102902e3dfa9b94ac1b08902f9
APRIL\april <beatle600@gmail.com>
2018-06-03 13:52:37 -0500

    Created new test project

---
+++ PetCareTest/Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PetCareTest
+{
+
+    [TestFixture]
+    public class Tutorial
+    {
+        [Test]
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
